d583770b6a6ab4eaea0a5b9ac8ea3236f609767e
ly <kakolp95@gmail.com>
2021-11-21 12:59:51 +0100

    Add a basic service

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Service\MascotService;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,8 +11,12 @@ class DefaultController extends AbstractController
 {
     #[Route("/")]
     #[Template("base.html.twig")]
-    public function index(): array
-    {
+    public function index(
+        MascotService $mascotService
+    ): array {
+        dump($mascotService);
+        dump($mascotService->getMascot());
+        dump($mascotService->getDirectories());
         return [];
     }
 }
---
+++ src/Service/MascotService.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Service;
+
+use Symfony\Component\Finder\Exception\DirectoryNotFoundException;
+use Symfony\Component\Finder\Finder;
+use Symfony\Component\Finder\SplFileInfo;
+use Symfony\Contracts\Cache\ItemInterface;
+use Symfony\Contracts\Cache\TagAwareCacheInterface;
+
+class MascotService
+{
+    public const TAG = 'mascot.service';
+
+    public function __construct(
+        private readonly TagAwareCacheInterface $cache,
+        private readonly string $MASCOT_PATH
+    ) {
+    }
+
+    public function getMascot(
+        array $paths = []
+    ): SplFileInfo|null {
+        dump($this->loadMascots());
+
+        return null;
+    }
+
+    public function getDirectories(): array
+    {
+        return $this->cache->get(self::TAG.'.mascots.full.directories', function (ItemInterface $item) {
+            $item->tag(self::TAG)
+                ->expiresAfter(1);
+
+            try {
+                return array_values(
+                    array_map(
+                        fn (SplFileInfo $f) => $f->getRelativePathname(),
+                        iterator_to_array((new Finder())->in($this->MASCOT_PATH)->directories())
+                    )
+                );
+            } catch (DirectoryNotFoundException) {
+                return [];
+            }
+        });
+    }
+
+    private function loadMascots(): array
+    {
+        return $this->cache->get(self::TAG.'.mascots.full', function (ItemInterface $item) {
+            $item->tag(self::TAG)
+                ->expiresAfter(15);
+
+            try {
+                return iterator_to_array((new Finder())->in(rtrim($this->MASCOT_PATH, '/\\').DIRECTORY_SEPARATOR.'*')->files());
+            } catch (DirectoryNotFoundException) {
+                return [];
+            }
+        });
+    }
+}
