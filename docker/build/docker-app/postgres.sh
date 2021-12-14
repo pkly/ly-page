d91b7028afa7b02153bc7f00c5ef6aa24897955c
ly <kakolp95@gmail.com>
2021-12-14 17:29:17 +0100

    Attempt to fix the init for postgres app-build

--- docker/build/docker-app/postgres.sh
+++ docker/build/docker-app/postgres.sh
@@ -252,41 +252,55 @@ docker_temp_server_stop() {
 	pg_ctl -D "$PGDATA" -m fast -w stop
 }
 
-init_postgres() {
-  docker_setup_env
-  # setup data directories and permissions (when run as root)
-  docker_create_db_directories
-  # only run initialization on an empty data directory
-  if [ -z "$DATABASE_ALREADY_EXISTS" ]; then
-    docker_verify_minimum_env
+_main() {
+	# if first arg looks like a flag, assume we want to run postgres server
+	if [ "${1:0:1}" = '-' ]; then
+		set -- postgres "$@"
+	fi
 
-    # check dir permissions to reduce likelihood of half-initialized database
-    ls /docker-entrypoint-initdb.d/ > /dev/null
+	if [ "$1" = 'postgres' ] && ! _pg_want_help "$@"; then
+		docker_setup_env
+		# setup data directories and permissions (when run as root)
+		docker_create_db_directories
+		if [ "$(id -u)" = '0' ]; then
+			# then restart script as postgres user
+			exec su-exec postgres "$BASH_SOURCE" "$@"
+		fi
 
-    docker_init_database_dir
-    pg_setup_hba_conf
+		# only run initialization on an empty data directory
+		if [ -z "$DATABASE_ALREADY_EXISTS" ]; then
+			docker_verify_minimum_env
 
-    # PGPASSWORD is required for psql when authentication is required for 'local' connections via pg_hba.conf and is otherwise harmless
-    # e.g. when '--auth=md5' or '--auth-local=md5' is used in POSTGRES_INITDB_ARGS
-    export PGPASSWORD="${PGPASSWORD:-$POSTGRES_PASSWORD}"
-    docker_temp_server_start
+			# check dir permissions to reduce likelihood of half-initialized database
+			ls /docker-entrypoint-initdb.d/ > /dev/null
 
-    docker_setup_db
-    docker_process_init_files /docker-entrypoint-initdb.d/*
+			docker_init_database_dir
+			pg_setup_hba_conf "$@"
 
-    docker_temp_server_stop
-    unset PGPASSWORD
+			# PGPASSWORD is required for psql when authentication is required for 'local' connections via pg_hba.conf and is otherwise harmless
+			# e.g. when '--auth=md5' or '--auth-local=md5' is used in POSTGRES_INITDB_ARGS
+			export PGPASSWORD="${PGPASSWORD:-$POSTGRES_PASSWORD}"
+			docker_temp_server_start "$@"
 
-    echo
-    echo 'PostgreSQL init process complete; ready for start up.'
-    echo
-  else
-    echo
-    echo 'PostgreSQL Database directory appears to contain a database; Skipping initialization'
-    echo
-  fi
+			docker_setup_db
+			docker_process_init_files /docker-entrypoint-initdb.d/*
 
-	postgres &
+			docker_temp_server_stop
+			unset PGPASSWORD
+
+			echo
+			echo 'PostgreSQL init process complete; ready for start up.'
+			echo
+		else
+			echo
+			echo 'PostgreSQL Database directory appears to contain a database; Skipping initialization'
+			echo
+		fi
+	fi
+
+	exec "$@" &
 }
 
-init_postgres
+if ! _is_sourced; then
+	_main "$@"
+fi
